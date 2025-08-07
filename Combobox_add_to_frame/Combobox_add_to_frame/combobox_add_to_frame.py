5c9217d3669e7928b0f2bbbe1c803bf589bbca8c
Hinkan <henrik.pet93@gmail.com>
2025-08-07 22:07:28 +0200

    add values from a combobox to a list

--- Combobox_add_to_frame/Combobox_add_to_frame/combobox_add_to_frame.py
+++ Combobox_add_to_frame/Combobox_add_to_frame/combobox_add_to_frame.py
@@ -1,14 +1,29 @@
 from tkinter import Frame, StringVar, Label, Button
 from tkinter.ttk import Combobox
+from Scroll_windows import ScrollFrame
 
 class combobox_add_to_frame:
-    def __init__(self, parent, list_selected, list_options):
+    """
+    Combobox that adds the selected items to a list below the combobox
+    Attributes:
+    """
+    def __init__(self, parent, list_selected, list_options, scrollable=False):
+        '''Parameters:
+            parent(Frame):parent frame
+            list_selected:the values added thorugh the combobox
+            list_options:The values for the combobox
+        '''
+        self.scrollable=scrollable
         self.frame=Frame(parent)
         self.var_selected=StringVar()
         self.list_selected=list_selected
         self.cbox=Combobox(self.frame, textvariable=self.var_selected, values=list_options)
         self.cbox.bind("<<ComboboxSelected>>", self._update_list)
         self.cbox.grid(row=0, column=0, columnspan=2)
+        if self.scrollable:
+            self.sf=ScrollFrame(self.frame)
+            self.sf.set_dimension((200,50))
+            self.sf.grid(row=1, column=0, columnspan=2)
     
     def get_list_selected(self):
         return self.list_selected
@@ -21,13 +36,26 @@ class combobox_add_to_frame:
         self._redraw()
         
     def _redraw(self):
-        for child in self.frame.winfo_children()[1:]:
-            child.destroy()
-        for row, sel in enumerate(self.list_selected):
-            l1=Label(self.frame, text=sel)
-            b1=Button(self.frame, text="X", command=lambda value=sel: self._delete_selected(value))
-            l1.grid(row=row+1, column=0)
-            b1.grid(row=row+1,column=1)
+        if self.scrollable:
+            for child in self.sf.scrollframe.winfo_children()[1:]:
+                child.destroy()
+            for row, sel in enumerate(self.list_selected):
+                rowframe=Frame(self.sf.scrollframe)
+            
+                l1=Label(rowframe, text=sel)
+                b1=Button(rowframe, text="X", command=lambda value=sel: self._delete_selected(value))
+                l1.grid(row=row+1, column=0, sticky="w")
+                b1.grid(row=row+1,column=1, sticky="e", padx=(10,0))
+                rowframe.pack(anchor="e")
+        else:
+            for child in self.frame.winfo_children()[1:]:
+                child.destroy()
+            for row, sel in enumerate(self.list_selected):
+                l1=Label(self.frame, text=sel)
+                b1=Button(self.frame, text="X", command=lambda value=sel: self._delete_selected(value))
+                l1.grid(row=row+1, column=0)
+                b1.grid(row=row+1,column=1)
+
 
     def _delete_selected(self, sel):
         idx=self.list_selected.index(sel)
@@ -40,3 +68,18 @@ class combobox_add_to_frame:
     def pack(self, **kwargs):
         self.frame.pack(kwargs)
 
+if __name__ == "__main__":
+    from tkinter import Tk, Button
+    root=Tk()
+    root.geometry("300x400")
+
+    selected=[]
+    values=["one", "two", "three"]
+
+    cb=combobox_add_to_frame(root, selected, values, scrollable=True)
+    cb.pack()
+
+    Button(root, text="read", command=lambda: print(selected)).pack()
+
+
+    root.mainloop()
